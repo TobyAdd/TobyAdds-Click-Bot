2ed180300e4dc0814dca62a2f4bdc0c39cbed5ed
TobyAdd <66429886+TobyAdd@users.noreply.github.com>
2021-11-16 23:43:27 +0200

    Project Files
    ye

---
+++ CBot/MainForm.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace CBot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TCB = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.CBot = new System.Windows.Forms.CheckBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.UpLabel = new System.Windows.Forms.Label();
+            this.DownLabel = new System.Windows.Forms.Label();
+            this.ClickGetter = new System.Windows.Forms.Timer(this.components);
+            this.button7 = new System.Windows.Forms.Button();
+            this.openDLLdialog = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TCB
+            // 
+            this.TCB.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.TCB.BackColor = System.Drawing.Color.DimGray;
+            this.TCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TCB.ForeColor = System.Drawing.Color.White;
+            this.TCB.Location = new System.Drawing.Point(1, 1);
+            this.TCB.Name = "TCB";
+            this.TCB.Size = new System.Drawing.Size(360, 21);
+            this.TCB.TabIndex = 0;
+            this.TCB.Text = "TobyAdd\'s Click Bot";
+            this.TCB.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.TCB.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            this.TCB.MouseMove += new System.Windows.Forms.MouseEventHandler(this.label1_MouseMove);
+            this.TCB.MouseUp += new System.Windows.Forms.MouseEventHandler(this.label1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(340, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(21, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "X";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(319, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(21, 21);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.CBot);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(4, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(155, 76);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TCB";
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(6, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(138, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Inject DLL";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // CBot
+            // 
+            this.CBot.AutoSize = true;
+            this.CBot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CBot.Location = new System.Drawing.Point(6, 19);
+            this.CBot.Name = "CBot";
+            this.CBot.Size = new System.Drawing.Size(80, 17);
+            this.CBot.TabIndex = 0;
+            this.CBot.Text = "Enable TCB";
+            this.CBot.UseVisualStyleBackColor = true;
+            this.CBot.CheckedChanged += new System.EventHandler(this.CBot_CheckedChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(4, 107);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(350, 111);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Hacks";
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Location = new System.Drawing.Point(6, 77);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(338, 23);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "Safe Mode [OFF]";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.Location = new System.Drawing.Point(6, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(338, 23);
+            this.button5.TabIndex = 1;
+            this.button5.Text = "Practice Music Hack [OFF]";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(6, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(338, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Noclip [OFF]";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.UpLabel);
+            this.groupBox3.Controls.Add(this.DownLabel);
+            this.groupBox3.ForeColor = System.Drawing.Color.White;
+            this.groupBox3.Location = new System.Drawing.Point(165, 25);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(189, 76);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Logs:";
+            // 
+            // UpLabel
+            // 
+            this.UpLabel.AutoSize = true;
+            this.UpLabel.Location = new System.Drawing.Point(6, 29);
+            this.UpLabel.Name = "UpLabel";
+            this.UpLabel.Size = new System.Drawing.Size(58, 13);
+            this.UpLabel.TabIndex = 1;
+            this.UpLabel.Text = "Release: 0";
+            // 
+            // DownLabel
+            // 
+            this.DownLabel.AutoSize = true;
+            this.DownLabel.Location = new System.Drawing.Point(6, 16);
+            this.DownLabel.Name = "DownLabel";
+            this.DownLabel.Size = new System.Drawing.Size(47, 13);
+            this.DownLabel.TabIndex = 0;
+            this.DownLabel.Text = "Down: 0";
+            // 
+            // ClickGetter
+            // 
+            this.ClickGetter.Interval = 1;
+            this.ClickGetter.Tick += new System.EventHandler(this.ClickGetter_Tick);
+            // 
+            // button7
+            // 
+            this.button7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button7.BackColor = System.Drawing.Color.DimGray;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(1, 1);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(57, 21);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "About";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // openDLLdialog
+            // 
+            this.openDLLdialog.FileName = "mydll.dll";
+            this.openDLLdialog.Filter = "DLL File (*.dll)|*.dll|All files (*.*)|*.*";
+            this.openDLLdialog.Title = "Open DLL...";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(362, 228);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TCB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.Text = "CBot";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainForm_Paint);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label TCB;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.CheckBox CBot;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label UpLabel;
+        private System.Windows.Forms.Label DownLabel;
+        private System.Windows.Forms.Timer ClickGetter;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.OpenFileDialog openDLLdialog;
+    }
+}
+
---
+++ CBot/MainForm.cs
@@ -0,0 +1,259 @@
+﻿using MemoryHacking;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace CBot
+
+{
+
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Keys vKeys);
+
+        private bool mouseDown;
+        private Point lastLocation;
+        int DownCounter;
+        int UpCounter;
+        bool DownBool = true;
+        bool UpBool = false;
+        String RandomDown;
+        String RandomUp;
+        String InjectStatus;
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Pen selPen = new Pen(Color.White);
+            g.DrawRectangle(selPen, 0, 0, this.Width - 1, this.Height - 1);
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDown = true;
+            lastLocation = e.Location;
+        }
+
+        private void label1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mouseDown)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X, (this.Location.Y - lastLocation.Y) + e.Y);
+
+                this.Update();
+            }
+        }
+
+        private void label1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseDown = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void CBot_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CBot.Checked == true)
+            {
+                ClickGetter.Enabled = true;
+            }
+            else
+            {
+                ClickGetter.Enabled = false;
+                DownLabel.Text = "Down: 0";
+                UpLabel.Text = "Release: 0";
+                DownCounter = 0;
+                UpCounter = 0;
+                
+            }
+        }
+
+
+
+        private void ClickGetter_Tick(object sender, EventArgs e)
+        {
+
+
+            if ((GetAsyncKeyState(Keys.LButton) & 0x8000) != 0)
+            {
+            if (DownBool == true)
+                {
+                    RandomDown = Directory.EnumerateFiles(@"tcb_down", "*.wav").Random();
+                    new Thread(playDownClicks).Start();
+                    DownBool = false;
+                    UpBool = true;
+                    DownCounter = DownCounter + 1;
+                    DownLabel.Text = "Down: " + DownCounter.ToString() + " (" + RandomDown + ")";
+                }
+            }
+            else
+            {
+                if (UpBool == true)
+                {
+                    RandomUp = Directory.EnumerateFiles(@"tcb_release", "*.wav").Random();
+                    new Thread(playReleaseClicks).Start();
+                    DownBool = true;
+                    UpBool = false;
+                    UpCounter = UpCounter + 1;
+                    UpLabel.Text = "Release: " + UpCounter.ToString() + " (" + RandomUp + ")";
+                }
+
+            }
+        }
+
+        private void playDownClicks()
+        {
+            SoundPlayer downSound = new SoundPlayer(@RandomDown);
+            downSound.Play();
+        }
+
+        private void playReleaseClicks()
+        {
+            SoundPlayer upSound = new SoundPlayer(@RandomUp);
+            upSound.Play();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (button4.Text == "Noclip [OFF]")
+            {
+                if (Process.GetProcessesByName("GeometryDash").Any())
+                {
+                    Process targetProcess = Process.GetProcessesByName("GeometryDash").First();
+                    Memory processMemory = targetProcess.InitializeMemory();
+                    IntPtr moduleBase = processMemory.GetModuleByName("GeometryDash.exe").BaseAddress;
+                    processMemory.Write(moduleBase + 0x20A1DD, new byte[] { 0xEB, 0x37 });
+                    button4.Text = "Noclip [ON]";
+                }
+            }
+            else if (button4.Text == "Noclip [ON]")
+            {
+                if (Process.GetProcessesByName("GeometryDash").Any())
+                {
+                    Process targetProcess = Process.GetProcessesByName("GeometryDash").First();
+                    Memory processMemory = targetProcess.InitializeMemory();
+                    IntPtr moduleBase = processMemory.GetModuleByName("GeometryDash.exe").BaseAddress;
+                    processMemory.Write(moduleBase + 0x20A1DD, new byte[] { 0x8A, 0x80 });
+                    button4.Text = "Noclip [OFF]";
+                }
+            }
+
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "Practice Music Hack [OFF]")
+            {
+                if (Process.GetProcessesByName("GeometryDash").Any())
+                {
+                    Process targetProcess = Process.GetProcessesByName("GeometryDash").First();
+                    Memory processMemory = targetProcess.InitializeMemory();
+                    IntPtr moduleBase = processMemory.GetModuleByName("GeometryDash.exe").BaseAddress;
+                    processMemory.Write(moduleBase + 0x20C925, new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90, 0x90 });
+                    processMemory.Write(moduleBase + 0x20D143, new byte[] { 0x90, 0x90 });
+                    processMemory.Write(moduleBase + 0x20A563, new byte[] { 0x90, 0x90 });
+                    processMemory.Write(moduleBase + 0x20A595, new byte[] { 0x90, 0x90 });
+                    button5.Text = "Practice Music Hack [ON]";
+                }
+            }
+            else if (button5.Text == "Practice Music Hack [ON]")
+            {
+                if (Process.GetProcessesByName("GeometryDash").Any())
+                {
+                    Process targetProcess = Process.GetProcessesByName("GeometryDash").First();
+                    Memory processMemory = targetProcess.InitializeMemory();
+                    IntPtr moduleBase = processMemory.GetModuleByName("GeometryDash.exe").BaseAddress;
+                    processMemory.Write(moduleBase + 0x20C925, new byte[] { 0x0F, 0x85, 0xF7, 0x00, 0x00, 0x00 });
+                    processMemory.Write(moduleBase + 0x20D143, new byte[] { 0x75, 0x41 });
+                    processMemory.Write(moduleBase + 0x20A563, new byte[] { 0x75, 0x3E });
+                    processMemory.Write(moduleBase + 0x20A595, new byte[] { 0x75, 0x0C });
+                    button5.Text = "Practice Music Hack [OFF]";
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "Safe Mode [OFF]")
+            {
+                if (Process.GetProcessesByName("GeometryDash").Any())
+                {
+                    Process targetProcess = Process.GetProcessesByName("GeometryDash").First();
+                    Memory processMemory = targetProcess.InitializeMemory();
+                    IntPtr moduleBase = processMemory.GetModuleByName("GeometryDash.exe").BaseAddress;
+                    processMemory.Write(moduleBase + 0x20A3B2, new byte[] { 0xE9, 0x9A, 0x01, 0x00, 0x00, 0x90, 0x90 });
+                    processMemory.Write(moduleBase + 0x1FD40F, new byte[] { 0xE9, 0x13, 0x06, 0x00, 0x00, 0x90, 0x90 });
+                    button6.Text = "Safe Mode [ON]";
+                }
+            }
+            else if (button6.Text == "Safe Mode [ON]")
+            {
+                if (Process.GetProcessesByName("GeometryDash").Any())
+                {
+                    Process targetProcess = Process.GetProcessesByName("GeometryDash").First();
+                    Memory processMemory = targetProcess.InitializeMemory();
+                    IntPtr moduleBase = processMemory.GetModuleByName("GeometryDash.exe").BaseAddress;
+                    processMemory.Write(moduleBase + 0x20A3B2, new byte[] { 0x80, 0xBB, 0x94, 0x04, 0x00, 0x00, 0x00 });
+                    processMemory.Write(moduleBase + 0x1FD40F, new byte[] { 0x83, 0xB9, 0x64, 0x03, 0x00, 0x00, 0x01 });
+                    button6.Text = "Safe Mode [OFF]";
+                }
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("TobyAdd's Click Bot v0.1!" + Environment.NewLine + "Thanks to:" + Environment.NewLine + "TobyAdd - Main Developer" + Environment.NewLine + "Eimaen - Main Developer 2 " + Environment.NewLine + "And you!", "About", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Process.GetProcessesByName("GeometryDash").Any())
+            {
+                if (openDLLdialog.ShowDialog() == DialogResult.OK)
+                {
+                    Process process = Process.GetProcessesByName("GeometryDash").First();
+                    process.InitializeMemory().Inject(openDLLdialog.FileName);
+                    MessageBox.Show("Injected!", "DLL Injector", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                   
+                }
+            }
+            else
+            {
+                MessageBox.Show("We cant inject dll, because gd is not running.", "Run GD lol", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+    public static class EnumerableExtensions
+    {
+        private static Random random = new Random();
+        public static T Random<T>(this IEnumerable<T> enumerable) => enumerable.ElementAt(random.Next(0, enumerable.Count()));
+    }
+}
+
